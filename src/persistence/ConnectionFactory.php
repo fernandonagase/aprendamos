3fc329c7520a832dc9e8228353d3f5a5fb3830df
Fernando Nagase <nagasef.fernando@gmail.com>
2019-12-13 00:04:13 -0300

    Adaptar banco de dados

--- src/persistence/ConnectionFactory.php
+++ src/persistence/ConnectionFactory.php
@@ -6,7 +6,7 @@ class ConnectionFactory
     public static function getConnection()
     {
         // host, username, password, database, (port)
-        $conn = mysqli_connect('localhost', 'aprendamos', 'aprendamos', 'aprendamos2', 3306);
+        $conn = mysqli_connect('localhost', 'aprendamos', 'aprendamos', 'aprendamos', 3306);
         if ($conn->connect_error) {
             die('Connect Error (' . $conn->connect_errno . ') ' . $conn->connect_error);
         }
